1a2e5ffc6c3b50009d523d7ee06c819ee97d0441
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-08-17 15:07:13 -0300

    setup inicial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui'],
+      'poppins': ['Poppins'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
