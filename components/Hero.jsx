d3d001dfe5c7924da5c3bc75d10784e5454f01ff
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-09-15 15:27:07 -0300

    height with calc - header

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const style = {
   wrapper: `relative`,
   container: `before:content-[''] -z-10 before:bg-red-500 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('/hero.png')] before:bg-cover before:bg-center before:opacity-40 before:blur-[1px]`,
-  contentWrapper: `relative flex flex-wrap items-center justify-center h-screen text-center md:text-start`,
+  contentWrapper: `relative flex flex-wrap items-center justify-center h-screen md:h-[calc(100vh-67px)] text-center md:text-start`,
   copyContainer: `md:w-1/2 md:px-4 md:px-0`,
   title: 'relative text-white text-5xl md:text-[46px] font-semibold md:leading-normal tracking-wide',
   description: `text-[#b2b8bd] z-10 container-[400px] text-2xl mt-[0.8rem] mb-[2.5rem]`,
